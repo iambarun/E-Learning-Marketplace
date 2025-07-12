0be70f2407c48f574dcdbbed3ec258eec0d47e68
barun bhattacharjee <bizs.barun@gmail.com>
2025-07-12 20:58:21 +0530

    custom css setup

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 const Index = () => {
   return (
     <>
-      <h1 className="text-center bg-primary text-white py-5 mb-4">Welcome to the Home Page</h1>
+      <h1 className="text-center py-5 mb-4 jumbotron">Welcome to the Home Page</h1>
       <p>This is the main page of our application using next js.</p>
     </>
   );
