4be19f34b6142dc18cecdf2992d6e8db5afe76ab
barun bhattacharjee <bizs.barun@gmail.com>
2025-07-12 21:14:35 +0530

    added login and registration pages with topnav component

---
+++ client/pages/register.js
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <>
+        <h1 className="jumbotron text-center py-5 mb-4">Register</h1>
+        <p className="text-center">This is registration page. Please enter your credentials to log in.</p>
+    </>
+  );
+}
+export default Register;
