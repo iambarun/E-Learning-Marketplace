89432ce2a13ac9f3f33b2941025f1b9b7e893de6
barun bhattacharjee <bizs.barun@gmail.com>
2025-07-12 12:38:20 +0530

    add initial local files

---
+++ client/pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main page of our application using next js.</p>
+    </div>
+  );
+};
+
+export default Index;
