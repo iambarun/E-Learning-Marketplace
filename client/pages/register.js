c8f7565bd2ddb578c1dfb63322ecac5f5ab6c443
barun bhattacharjee <bizs.barun@gmail.com>
2025-07-25 23:03:54 +0530

    register page

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,8 +1,31 @@
+import { useState } from 'react';
+
+
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.table({ name, email, password });
+  }
   return (
     <>
         <h1 className="jumbotron text-center py-5 mb-4">Register</h1>
-        <p className="text-center">This is registration page. Please enter your credentials to log in.</p>
+        
+        <div className="container col-md-4 offset-md-4 pb-5">
+          <form onSubmit={handleSubmit}>
+            <input type="text" className="form-control mb-4 p-3" value={name} onChange={(e) => setName(e.target.value)} placeholder='userName' required />
+            <input type="email" className="form-control mb-4 p-3" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' required />
+            <input type="password" className="form-control mb-4 p-3" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' required />
+
+            <br />
+            <button type='submit' className="btn w-50 btn-primary  p-3">Submit</button>
+            <button type='cancel' className="btn w-50 btn-success  p-3">Cancel</button>
+          </form>
+        </div>
     </>
   );
 }
